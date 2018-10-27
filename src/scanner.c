2918bc1e462279b8204b1c66eac87e4158c04f78
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2018-10-27 16:20:47 +0300

    fix minor scanner bugs

--- src/scanner.c
+++ src/scanner.c
@@ -57,7 +57,7 @@ static bool match(const char expected) {
 		return false;
 
 	advance();
-	return false;
+	return true;
 }
 
 static bool isDigit(const char c) {
@@ -129,8 +129,8 @@ static TokenType identifierType() {
 		case 't': {
 			if (scanner.current - scanner.start > 1) {
 				switch(scanner.start[1]) {
-					case 'h': return checkKeyword(2, 2, "is", TOKEN_FALSE);
-					case 'r': return checkKeyword(2, 2, "ue", TOKEN_FOR);
+					case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
+					case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
 				}
 			}
 		}

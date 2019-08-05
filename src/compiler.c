575a52de93338fcb94aa01def09f7029ef0bf6cc
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2019-08-05 23:20:51 +0200

    compile 'and' and 'or' logical operators

--- src/compiler.c
+++ src/compiler.c
@@ -387,6 +387,22 @@ static void declaration() {
 	if (parser.panic_mode) synchronize();
 }
 
+static void and_(bool can_assign) {
+	int jump = emitJump(OP_JUMP_IF_FALSE);
+	emitByte(OP_POP);
+	parsePrecedence(PREC_AND);
+	patchJump(jump);
+}
+
+static void or_(bool can_assign) {
+	int jump = emitJump(OP_JUMP_IF_FALSE);
+	int end_jump = emitJump(OP_JUMP);
+	patchJump(jump);
+	emitByte(OP_POP);
+	parsePrecedence(PREC_OR);
+	patchJump(end_jump);
+}
+
 static void literal(bool can_assign) {
 	switch (parser.previous.type) {
 		case TOKEN_NIL: emitByte(OP_NIL); break;
@@ -510,7 +526,7 @@ ParseRule rules[] = {
 	{ variable, NULL,    PREC_NONE },       // TOKEN_IDENTIFIER
 	{ string,   NULL,    PREC_NONE },       // TOKEN_STRING
 	{ number,   NULL,    PREC_NONE },       // TOKEN_NUMBER
-	{ NULL,     NULL,    PREC_AND },        // TOKEN_AND
+	{ NULL,     and_,    PREC_AND },        // TOKEN_AND
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE
 	{ literal,  NULL,    PREC_NONE },       // TOKEN_FALSE
@@ -518,7 +534,7 @@ ParseRule rules[] = {
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_FOR
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_IF
 	{ literal,  NULL,    PREC_NONE },       // TOKEN_NIL
-	{ NULL,     NULL,    PREC_OR },         // TOKEN_OR
+	{ NULL,     or_,    PREC_OR },         // TOKEN_OR
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER
